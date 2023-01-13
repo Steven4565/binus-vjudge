e08dcbc54baf283618eaab7bea7f660ca13da594
steven <stevenchristian4565@gmail.com>
2023-01-13 21:23:38 +0700

    added documentation for OLQ10 b

--- LabBC02-BC02-ONLINE QUIZ #10/b.c
+++ LabBC02-BC02-ONLINE QUIZ #10/b.c
@@ -13,43 +13,42 @@ int main()
 	{
 		char input[101];
 
+		// get the full string
 		fscanf(handle, "%[^\n]\n", &input);
 		int length = strlen(input);
 
 		int queries;
 		fscanf(handle, "%d\n", &queries);
 
+		// map to track which characters has been changed
 		bool sourcesChanged[26] = {0};
+
 		for (int i = 0; i < queries; i++)
 		{
+			// get the input
 			char source, dest;
 			fscanf(handle, "%c %c\n", &source, &dest);
+
+			// if character has already been changed, skip
 			if (sourcesChanged[source - 'A'])
 				continue;
 			sourcesChanged[source - 'A'] = true;
 
 			// change every occurance of string
 			for (int i = 0; i < length; i++)
-			{
 				if (input[i] == source)
-				{
 					input[i] = dest;
-				}
-			}
-		}
-		int count[26] = {0};
-		for (int i = 0; i < length; i++)
-		{
-			count[input[i] - 'A']++;
 		}
 
+		// count the letters
+		int count[26] = {0};
+		for (int i = 0; i < length; i++)
+			count[input[i] - 'A']++;
+
+		// print it out
 		for (int i = 0; i < 26; i++)
-		{
 			if (count[i])
-			{
 				printf("%c %d\n", i + 'A', count[i]);
-			}
-		}
 	}
 
 	fclose(handle);
--- LabBC02-BC02-ONLINE QUIZ #10/d.c
+++ LabBC02-BC02-ONLINE QUIZ #10/d.c
@@ -9,42 +9,37 @@
 int main()
 {
 
+	FILE *handle = fopen("testdata.in", "r");
 	int test;
-	scanf(" %d", &test);
+	fscanf(handle, "%d\n", &test);
 
 	for (int t = 0; t < test; t++)
 	{
 		int n;
-		scanf(" %d", &n);
+		fscanf(handle, "%d\n", &n);
 
 		int min, max;
-		scanf(" %d %d", &min, &max);
+		fscanf(handle, "%d %d\n", &min, &max);
 
-		int counter = 0;
 		int valid = 0;
-		bool flag = false;
+		int prev0 = -1;
 		for (int i = 0; i < n; i++)
 		{
 			char buffer;
-			scanf(" %c", &buffer);
-			if (buffer == '1')
+			fscanf(handle, " %c", &buffer);
+			if (buffer == '0')
 			{
-				counter++;
-			}
-			else
-			{
-				counter = 0;
-				flag = false;
-			}
-			if (counter >= min && counter <= max && !valid)
-			{
-				valid++;
-				flag = true;
+				int consecutives = i - prev0 - 1;
+				if (consecutives >= min && consecutives <= max)
+					valid++;
+				prev0 = i;
 			}
 		}
 
 		printf("Case #%d: %d\n", t + 1, valid);
 	}
+
+	fclose(handle);
 
 	return 0;
 }
